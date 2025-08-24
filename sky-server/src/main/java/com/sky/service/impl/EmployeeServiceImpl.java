8fffc5cb2fb58e37b05f945280df810676c6fe8d
(° -° )つ口 <1433675821@qq.com>
2025-08-24 16:42:53 +0800

    启用禁用员工账号代码

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -90,4 +90,15 @@ public class EmployeeServiceImpl implements EmployeeService {
         List<Employee> records = page.getResult();
         return new  PageResult(total,records);
     }
+
+
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = Employee.builder()
+                .id(id)
+                .status(status)
+                .build();
+
+        employeeMapper.update(employee);
+    }
 }
